47ebb855fd5a888e36166ca816b3ce6045af6183
paul_huang <paulhuang0056@gmail.com>
2020-03-17 11:30:48 +0800

    adjustment search view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
-
-function App() {
+const App = () => {
+  const [searchVal, setSearchVal] = useState("");
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [groupCount] = useState(6);
+  const [totalPage, setTotalPage] = useState(0);
+  useEffect(() => {
+    if(items.length === 0) getDate();
+    setTotalPage(items.length / groupCount);
+  });
+  const getDate = () => {
+    axios.get("https://www.googleapis.com/youtube/v3/search", {
+      params: {
+        key: "AIzaSyCR1LSnj1RrWWuT6adS4YOLRUd2xJTirp4",
+        part: "snippet",
+        type: "video",
+        maxResults: 18,
+        q: searchVal
+      }
+    })
+    .then(res => setItems(res.data.items))
+  };
+  const renderItem = (item) => {
+    const title = item.snippet.title;
+    const videoId = item.id.videoId;
+    const imgUrl = item.snippet.thumbnails.medium.url;
+    return (
+      <li className="ytb_item" key={videoId}>
+        <a href={`https://www.youtube.com/watch?v=${videoId}`} target="_blank">
+          <img src={imgUrl}/>
+          <h2>{title}</h2>
+        </a>
+      </li>
+    )
+  };
+  const filterItem = (items) => {
+    const maxCunt = (groupCount * currentPage);
+    const minCunt = maxCunt - groupCount;
+    return items.filter((item, index) => index >= minCunt && index <= maxCunt - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="container">
+          <div className="search_block">
+            <input type="text" className="search" value={searchVal} onChange={(e) => setSearchVal(e.target.value)}/>
+            <button className="search_btn" onClick={() => setItems([])}><FontAwesomeIcon icon={faSearch} /></button>
+          </div>
+        </div>
       </header>
+      <section>
+        <ul className="ytb_lists container">
+          {filterItem(items).map(item => renderItem(item))}
+        </ul>
+        <div className="pages">
+          {Array.from({length: totalPage}, (v, i) => i+1).map(item => <span key={`page_num_${item}`} className={[currentPage === item ? "page_num active" : "page_num"]} onClick={() => setCurrentPage(item)}>{item}</span>)}
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
